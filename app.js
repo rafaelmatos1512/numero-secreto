6d41acc753fcfce74b61b0e8856b1ab1abaa13de
rafaelmatos1512 <rafaellima151204@gmail.com>
2025-03-18 16:11:24 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,41 @@
+let numeroSecreto =  gerarNumeroAleatorio();
+let tentativas = 5;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um númuro entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+  
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Voce descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        ument.
+
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O numero secrecto é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O numero secrecto é maior!');
+        }
+        tentativas++;
+        LimparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function LimparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+
+}
